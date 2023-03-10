d04b0eca5f6d56eeaf4b164a99797ece2d80c6eb
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2023-03-10 01:23:10 -0800

    added handleChange function for form

--- pages/form.js
+++ pages/form.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Layout from "../components/layout";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function Form() {
   const [name, setName] = useState("");
@@ -9,11 +9,24 @@ export default function Form() {
   const [checked, setChecked] = useState(false);
 
   function handleChange(event) {
-
+    console.log(event.target.checked);
+    switch (event.target.id) {
+      case "form-name":
+        setName(event.target.value)
+        break;
+      case "form-email":
+        setEmail(event.target.value)
+        break;
+      case "form-message":
+        setMessage(event.target.value)
+        break;
+      case "form-checkbox":
+        setChecked(event.target.checked)
+    }
   }
 
   function handleSubmit(event) {
-
+    // submit to what exactly? 
   }
 
   return (
@@ -28,13 +41,13 @@ export default function Form() {
             <input
               type="text"
               className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-              id="exampleInput7"
+              id="form-name"
               placeholder="Name"
               value={name}
-              onChange={}
+              onChange={handleChange}
             />
             <label
-              for="exampleInput7"
+              for="form-name"
               className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
             >
               Name
@@ -44,28 +57,29 @@ export default function Form() {
             <input
               type="email"
               className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-              id="exampleInput8"
+              id="form-email"
               placeholder="Email address"
               value={email}
-              onChange={}
+              onChange={handleChange}
             />
             <label
-              for="exampleInput8"
+              for="form-email"
               className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
             >
               Email address
             </label>
           </div>
           <div className="relative mb-6" data-te-input-wrapper-init>
-            <input
+            <textarea
               className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-              id="exampleFormControlTextarea13"
+              id="form-message"
               placeholder="message"
+              rows={3}
               value={message}
-              onChange={}
-            ></input>
+              onChange={handleChange}
+            ></textarea>
             <label
-              for="exampleFormControlTextarea13"
+              for="form-message"
               className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
             >
               Message
@@ -75,12 +89,13 @@ export default function Form() {
             <input
               className="relative float-left mr-[6px] -ml-[1.5rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 dark:border-neutral-600 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary dark:checked:border-primary checked:bg-primary dark:checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:ml-[0.25rem] checked:after:-mt-px checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-t-0 checked:after:border-l-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:ml-[0.25rem] checked:focus:after:-mt-px checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-t-0 checked:focus:after:border-l-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent"
               type="checkbox"
-              value={checked}
-              id="exampleCheck10"
+              checked={checked}
+              id="form-checkbox"
+              onChange={handleChange}
             />
             <label
               className="inline-block pl-[0.15rem] hover:cursor-pointer"
-              for="exampleCheck10"
+              for="form-checkbox"
             >
               Send me a copy of this message
             </label>
