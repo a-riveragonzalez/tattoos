c190b29cc9ed40ef012153ff917b5e7b6e155e7e
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2023-03-12 01:04:57 -0800

    added to gallery

--- components/gallery.js
+++ components/gallery.js
@@ -2,65 +2,62 @@ export default function Gallery() {
   return (
     <>
       {/* ************************* option 1 ************************* */}
-
-      <section className="overflow-hidden text-neutral-700">
-        <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
-          <div className="-m-1 flex flex-wrap md:-m-2">
-            <div className="flex w-1/3 flex-wrap">
-              <div className="w-full p-1 md:p-2">
-                <img
-                  alt="gallery"
-                  className="block h-full w-full rounded-lg object-cover object-center"
-                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-                />
-              </div>
-            </div>
-            <div className="flex w-1/3 flex-wrap">
-              <div className="w-full p-1 md:p-2">
-                <img
-                  alt="gallery"
-                  className="block h-full w-full rounded-lg object-cover object-center"
-                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(74).webp"
-                />
-              </div>
-            </div>
-            <div className="flex w-1/3 flex-wrap">
-              <div className="w-full p-1 md:p-2">
-                <img
-                  alt="gallery"
-                  className="block h-full w-full rounded-lg object-cover object-center"
-                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(75).webp"
-                />
-              </div>
-            </div>
-            <div className="flex w-1/3 flex-wrap">
-              <div className="w-full p-1 md:p-2">
-                <img
-                  alt="gallery"
-                  className="block h-full w-full rounded-lg object-cover object-center"
-                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(70).webp"
-                />
-              </div>
-            </div>
-            <div className="flex w-1/3 flex-wrap">
-              <div className="w-full p-1 md:p-2">
-                <img
-                  alt="gallery"
-                  className="block h-full w-full rounded-lg object-cover object-center"
-                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(76).webp"
-                />
-              </div>
-            </div>
-            <div className="flex w-1/3 flex-wrap">
-              <div className="w-full p-1 md:p-2">
-                <img
-                  alt="gallery"
-                  className="block h-full w-full rounded-lg object-cover object-center"
-                  src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
-                />
-              </div>
-            </div>
-          </div>
+      <section class="grid grid-rows-1 md:grid-rows-2 lg:grid-rows-3 grid-flow-col gap-4">
+        <div>
+          <img
+            alt="gallery"
+            className="block h-full w-full rounded-lg object-cover object-center"
+            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+          />
+        </div>
+        <div>
+          <img
+            alt="gallery"
+            className="block h-full w-full rounded-lg object-cover object-center"
+            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+          />
+        </div>
+        <div>
+          <img
+            alt="gallery"
+            className="block h-full w-full rounded-lg object-cover object-center"
+            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+          />
+        </div>
+        <div>
+          <img
+            alt="gallery"
+            className="block h-full w-full rounded-lg object-cover object-center"
+            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+          />
+        </div>
+        <div>
+          <img
+            alt="gallery"
+            className="block h-full w-full rounded-lg object-cover object-center"
+            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+          />
+        </div>
+        <div>
+          <img
+            alt="gallery"
+            className="block h-full w-full rounded-lg object-cover object-center"
+            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+          />
+        </div>
+        <div>
+          <img
+            alt="gallery"
+            className="block h-full w-full rounded-lg object-cover object-center"
+            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+          />
+        </div>
+        <div>
+          <img
+            alt="gallery"
+            className="block h-full w-full rounded-lg object-cover object-center"
+            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+          />
         </div>
       </section>
     </>
