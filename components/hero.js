17f3a694a3771e1d86cd47b2c2cc7a2ea55ef17b
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2023-03-21 14:28:11 -0700

    added parallax to hero section

--- components/hero.js
+++ components/hero.js
@@ -1,18 +1,31 @@
+import { Parallax } from "react-parallax";
+
 export default function Hero() {
   return (
-    <section className="w-full h-full pb-12">
-      <div className="container mx-auto relative bg-[url('/images/hero.jpg')] bg-center bg-cover max-w-5xl">
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="text-center ">
-            <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-              <span className="bg-black/[.20] px-2">Tattoo Artist</span>
-            </h1>
-            <p className="mt-5 text-lg leading-8 text-white">
-              <span className="bg-black/[.60] p-1">Located in Los Angeles, CA</span>
-            </p>
+    <>
+      <Parallax
+        bgImage='/images/hero.jpg'
+        bgImageAlt="city that glows in pink and light blue"
+        strength={800}
+        className="home-image"
+      >
+        <section className="w-full h-full pb-12">
+          <div className="container mx-auto relative max-w-5xl">
+            <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+              <div className="text-center ">
+                <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+                  <span className="bg-black/[.20] px-2">Tattoo Artist</span>
+                </h1>
+                <p className="mt-5 text-lg leading-8 text-white">
+                  <span className="bg-black/[.60] p-1">
+                    Located in Los Angeles, CA
+                  </span>
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </section>
+        </section>
+      </Parallax>
+    </>
   );
 }
