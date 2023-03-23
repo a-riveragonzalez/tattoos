9f052010519d6738182fa6d03c52ab473ff3e918
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2023-03-22 17:57:06 -0700

    fixed the gallery columns

--- components/gallery.js
+++ components/gallery.js
@@ -1,58 +1,58 @@
 export default function Gallery() {
   return (
     <>
-      {/* ************************* option 1 ************************* */}
-      <section class="grid grid-rows-1 md:grid-rows-2 lg:grid-rows-3 grid-flow-col gap-4">
-        <div>
+      <h2 className="text-white text-center">look at this thing, isn't it neat </h2>
+      <section className="columns-1 md:columns-2 xl:columns-3 py-10 pb-20 px-8">
+        <div className="pb-5">
           <img
             alt="gallery"
             className="block h-full w-full rounded-lg object-cover object-center"
             src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
           />
         </div>
-        <div>
+        <div className="pb-5">
           <img
             alt="gallery"
             className="block h-full w-full rounded-lg object-cover object-center"
             src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
           />
         </div>
-        <div>
+        <div className="pb-5">
           <img
             alt="gallery"
             className="block h-full w-full rounded-lg object-cover object-center"
             src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
           />
         </div>
-        <div>
+        <div className="pb-5">
           <img
             alt="gallery"
             className="block h-full w-full rounded-lg object-cover object-center"
             src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
           />
         </div>
-        <div>
+        <div className="pb-5">
           <img
             alt="gallery"
             className="block h-full w-full rounded-lg object-cover object-center"
             src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
           />
         </div>
-        <div>
+        <div className="pb-5">
           <img
             alt="gallery"
             className="block h-full w-full rounded-lg object-cover object-center"
             src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
           />
         </div>
-        <div>
+        <div className="pb-5">
           <img
             alt="gallery"
             className="block h-full w-full rounded-lg object-cover object-center"
             src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
           />
         </div>
-        <div>
+        <div className="">
           <img
             alt="gallery"
             className="block h-full w-full rounded-lg object-cover object-center"
