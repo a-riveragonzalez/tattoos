d00aef6ada242c173af17b5ca8caf1534cce8fb4
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2023-04-01 01:30:31 -0700

    added useState for checkbox, added logic to have the pictures displayed change

--- components/gallery.js
+++ components/gallery.js
@@ -1,8 +1,12 @@
+import React, { useState } from "react";
+
 export default function Gallery() {
+  const [isTat, setIsTat] = useState(false);
+
   return (
     <>
-      <h2 className="text-center mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
-        look at this thing, isn't it neat{" "}
+      <h2 className="text-center pl-2 mt-2 text-3xl font-bold tracking-tight text-white sm:text-4xl">
+        Work | Gallery
       </h2>
 
       {/* toggle  */}
@@ -11,7 +15,13 @@ export default function Gallery() {
           for="Toggle3"
           className="inline-flex items-center p-2 rounded-md cursor-pointer text-gray-800 "
         >
-          <input id="Toggle3" type="checkbox" className="hidden peer" />
+          <input
+            id="Toggle3"
+            type="checkbox"
+            className="hidden peer"
+            checked={isTat}
+            onChange={(e) => setIsTat(e.target.checked)}
+          />
           <span className="px-4 py-2 rounded-l-md bg-blue-400 peer-checked:bg-gray-300">
             Tattoos
           </span>
@@ -22,64 +32,126 @@ export default function Gallery() {
       </div>
 
       {/* this will be a if then for tattoos or drawings */}
-      <section className="columns-1 md:columns-2 xl:columns-3 py-10 pb-20 px-8">
-        <div className="pb-5">
-          <img
-            alt="gallery"
-            className="block h-full w-full rounded-lg object-cover object-center"
-            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-          />
-        </div>
-        <div className="pb-5">
-          <img
-            alt="gallery"
-            className="block h-full w-full rounded-lg object-cover object-center"
-            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-          />
-        </div>
-        <div className="pb-5">
-          <img
-            alt="gallery"
-            className="block h-full w-full rounded-lg object-cover object-center"
-            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-          />
-        </div>
-        <div className="pb-5">
-          <img
-            alt="gallery"
-            className="block h-full w-full rounded-lg object-cover object-center"
-            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-          />
-        </div>
-        <div className="pb-5">
-          <img
-            alt="gallery"
-            className="block h-full w-full rounded-lg object-cover object-center"
-            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-          />
-        </div>
-        <div className="pb-5">
-          <img
-            alt="gallery"
-            className="block h-full w-full rounded-lg object-cover object-center"
-            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-          />
-        </div>
-        <div className="pb-5">
-          <img
-            alt="gallery"
-            className="block h-full w-full rounded-lg object-cover object-center"
-            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-          />
-        </div>
-        <div className="">
-          <img
-            alt="gallery"
-            className="block h-full w-full rounded-lg object-cover object-center"
-            src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
-          />
-        </div>
-      </section>
+
+      {isTat ? (
+        <section className="columns-1 md:columns-2 xl:columns-3 py-10 pb-20 px-8">
+          <div className="pb-5">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+            />
+          </div>
+          <div className="pb-5">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+            />
+          </div>
+          <div className="pb-5">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+            />
+          </div>
+          <div className="pb-5">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+            />
+          </div>
+          <div className="pb-5">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+            />
+          </div>
+          <div className="pb-5">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+            />
+          </div>
+          <div className="pb-5">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+            />
+          </div>
+          <div className="">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+            />
+          </div>
+        </section>
+      ) : (
+        <section className="columns-1 md:columns-2 xl:columns-3 py-10 pb-20 px-8">
+          <div className="pb-5">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+            />
+          </div>
+          <div className="pb-5">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+            />
+          </div>
+          <div className="pb-5">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+            />
+          </div>
+          <div className="pb-5">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+            />
+          </div>
+          <div className="pb-5">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+            />
+          </div>
+          <div className="pb-5">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+            />
+          </div>
+          <div className="pb-5">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+            />
+          </div>
+          <div className="">
+            <img
+              alt="gallery"
+              className="block h-full w-full rounded-lg object-cover object-center"
+              src="https://tecdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(72).webp"
+            />
+          </div>
+        </section>
+      )}
     </>
   );
 }
