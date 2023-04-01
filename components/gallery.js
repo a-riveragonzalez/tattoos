2e7d30247543e339138a8a5f574e8ec508d52867
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2023-04-01 00:10:07 -0700

    changed toggle labels

--- components/gallery.js
+++ components/gallery.js
@@ -13,10 +13,10 @@ export default function Gallery() {
         >
           <input id="Toggle3" type="checkbox" className="hidden peer" />
           <span className="px-4 py-2 rounded-l-md dark:bg-violet-400 peer-checked:dark:bg-gray-300">
-            Monthly
+            Tattoos
           </span>
           <span className="px-4 py-2 rounded-r-md dark:bg-gray-300 peer-checked:dark:bg-violet-400">
-            Annually
+            Drawings
           </span>
         </label>
       </div>
