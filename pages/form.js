c00baafe2cf0c5b64c01e77e90936d41dd4ab53b
Arely Rivera <arelyrivera1996@gmail.com>
2023-05-06 15:50:50 -0700

    fixed radio for form

--- pages/form.js
+++ pages/form.js
@@ -10,7 +10,7 @@ export default function Form() {
   const [tatDes, setTatDes] = useState("");
   const [approxSize, setApproxSize] = useState("");
   const [placement, setPlacement] = useState("");
-  // const [age, setAge] = useState(false);
+  const [option, setOption] = useState(null);
   // upload reference
   // upload placement
 
@@ -45,8 +45,13 @@ export default function Form() {
         setPlacement(event.target.value);
         console.log(event.target.value);
         break;
-      case "push-age":
-        console.log(event);
+      case "option-yes":
+        setOption(event.target.value);
+        console.log(event.target.value);
+        break;
+      case "option-no":
+        setOption(event.target.value);
+        console.log(event.target.value);
         break;
     }
   }
@@ -80,7 +85,7 @@ export default function Form() {
                 autoComplete="first-name"
                 value={firstName}
                 onChange={handleChange}
-                className="mt-2 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="mt-2 block pl-1 w-full text-black rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
 
@@ -99,11 +104,11 @@ export default function Form() {
                 autoComplete="last-name"
                 value={lastName}
                 onChange={handleChange}
-                className="mt-2 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="mt-2 block w-full pl-1 text-black rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
 
-            {/* ?????? TO DO ??????? over 18 div */}
+            {/* over 18 div */}
             <div className="flex items-center py-2">
               <div
                 className="text-sm font-semibold leading-6 pr-3 "
@@ -112,33 +117,24 @@ export default function Form() {
                 Are you over 18?
               </div>
 
-              <input
-                id="push-age"
-                name="push-age"
-                type="checkbox"
-                onClick={handleChange}
-                value={true}
-                className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-              />
-              <label
-                for="yes"
-                className="ml-3 block text-sm font-medium leading-6 pr-3"
-              >
+              <label>
+                <input
+                  type="radio"
+                  value="yes"
+                  id="option-yes"
+                  checked={option === "yes"}
+                  onChange={handleChange}
+                />
                 Yes
               </label>
-
-              <input
-                id="push-age"
-                name="push-age"
-                type="radio"
-                onClick={handleChange}
-                value={false}
-                className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
-              />
-              <label
-                for="no"
-                className="ml-3 block text-sm font-medium leading-6"
-              >
+              <label>
+                <input
+                  type="radio"
+                  value="no"
+                  id="option-no"
+                  checked={option === "no"}
+                  onChange={handleChange}
+                />
                 No
               </label>
             </div>
@@ -158,7 +154,7 @@ export default function Form() {
                 autoComplete="email"
                 value={email}
                 onChange={handleChange}
-                className="mt-2 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                className="mt-2 block pl-1 w-full text-black rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
 
@@ -177,16 +173,13 @@ export default function Form() {
                 autoComplete="phone-num"
                 value={phoneNum}
                 onChange={handleChange}
-                class="mt-2 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                class="mt-2 block pl-1 w-full text-black rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
 
             {/* tattoo description div */}
             <div class="">
-              <label
-                for="tat-des"
-                class="block text-sm font-medium leading-6"
-              >
+              <label for="tat-des" class="block text-sm font-medium leading-6">
                 Tattoo Description
               </label>
               <input
@@ -196,7 +189,7 @@ export default function Form() {
                 autoComplete="tat-des"
                 value={tatDes}
                 onChange={handleChange}
-                class="mt-2 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                class="mt-2 pl-1 block w-full text-black rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
 
@@ -256,7 +249,7 @@ export default function Form() {
                 autoComplete="approx-size"
                 value={approxSize}
                 onChange={handleChange}
-                class="mt-2 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                class="mt-2 block w-full pl-1 text-black rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
 
@@ -264,7 +257,7 @@ export default function Form() {
             <div class="">
               <label
                 for="placement"
-                class="block text-sm font-medium leading-6"
+                class="block text-sm font-medium leading-6 "
               >
                 Placement
               </label>
@@ -275,54 +268,13 @@ export default function Form() {
                 autoComplete="placement"
                 value={placement}
                 onChange={handleChange}
-                class="mt-2 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                class="mt-2 block w-full pl-1 text-black rounded-md border-0 py-1.5 mb-5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
-            </div>
-
-            {/* ?????? TO DO ??????? Upload a picture of placement */}
-            <div>
-              <label class="block text-sm font-medium leading-6">
-                Upload a picture of placement
-              </label>
-              <div class="mt-2 flex justify-center rounded-md border-2 border-dashed bg-white px-6 pt-5 pb-6">
-                <div class="space-y-1 text-center">
-                  <svg
-                    class="mx-auto h-12 w-12 text-gray-400"
-                    stroke="currentColor"
-                    fill="none"
-                    viewBox="0 0 48 48"
-                    aria-hidden="true"
-                  >
-                    <path
-                      d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
-                  <div class="flex text-sm text-gray-600">
-                    <label
-                      for="file-upload"
-                      class="relative cursor-pointer rounded-md bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
-                    >
-                      <span>Upload a file</span>
-                      <input
-                        id="file-upload"
-                        name="file-upload"
-                        type="file"
-                        class="sr-only"
-                      />
-                    </label>
-                    <p class="pl-1">or drag and drop</p>
-                  </div>
-                  <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-                </div>
-              </div>
             </div>
           </div>
 
           {/* submit button */}
-          <div class="bg-gray-200 px-4 py-3 text-right sm:px-6 ">
+          <div class="bg-gray-200 px-4 py-3 text-center sm:px-6 ">
             <button
               type="submit"
               class="inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
@@ -353,24 +305,24 @@ export default function Form() {
 
 //   return (
 //     <form onSubmit={handleSubmit}>
-//       <label>
-//         <input
-//           type="radio"
-//           value="yes"
-//           checked={option === "yes"}
-//           onChange={handleOptionChange}
-//         />
-//         Yes
-//       </label>
-//       <label>
-//         <input
-//           type="radio"
-//           value="no"
-//           checked={option === "no"}
-//           onChange={handleOptionChange}
-//         />
-//         No
-//       </label>
+// <label>
+//   <input
+//     type="radio"
+//     value="yes"
+//     checked={option === "yes"}
+//     onChange={handleOptionChange}
+//   />
+//   Yes
+// </label>
+// <label>
+//   <input
+//     type="radio"
+//     value="no"
+//     checked={option === "no"}
+//     onChange={handleOptionChange}
+//   />
+//   No
+// </label>
 //       <button type="submit">Submit</button>
 //     </form>
 //   );
