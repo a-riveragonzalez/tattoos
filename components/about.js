d46749e20d04fa8f77d708971fe70cebd099e551
Arely Rivera <arelyrivera1996@gmail.com>
2023-05-24 22:10:34 -0700

    fixed sizing in the image for about section

--- components/about.js
+++ components/about.js
@@ -62,7 +62,7 @@ export default function About() {
             <img
               src="/images/horizontal-tattoo.jpg"
               alt="Product screenshot"
-              className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+              className="w-[48rem] md:max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
               // className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
               // width="2432"
               // height="1442"
